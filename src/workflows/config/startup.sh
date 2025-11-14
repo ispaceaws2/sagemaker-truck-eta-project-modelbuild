1f4dab2907add1aca42566c70db4890b17762131
Revanth <arn:aws:iam::316348991122:user/Revanth>
2025-11-14 07:33:54 +0000

    Added src folder as normal directory (submodule removed)

---
+++ src/workflows/config/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Place any special instructions you'd like run during your workflows environment startup here
+# Note that you will need to restart your space for changes to take effect.
+# For example:
+# pip install dbt-core
